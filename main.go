f6258b5596dd2bba7c97c7d4f5a2f555b98d25bf
Daniel Dutra <93202813+DanielDutraMl@users.noreply.github.com>
2022-03-10 15:07:36 -0300

    first

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/DanielDutraMl/Go-Web/models"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
